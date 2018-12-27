3b0eabd68b64ed13d7d5a09367b669ed43a96783
kang yoosam <yooska14@gmail.com>
2018-12-27 19:50:23 +0900

    delete HOST_PRODUCTION

--- src/api/index.js
+++ src/api/index.js
@@ -1,13 +1,7 @@
-import { Platform } from 'react-native'
-import { API_HOST, API_ENTRY_POINT, API_HOST_PRODUCTION } from 'react-native-dotenv'
+import { API_HOST, API_ENTRY_POINT } from 'react-native-dotenv'
 import axios from 'axios'
 
-let HOST = API_HOST
-if (Platform.OS === 'android' && __DEV__) {
-  HOST = API_HOST_PRODUCTION
-}
-
-const BASE_URL = `${HOST}${API_ENTRY_POINT}`
+const BASE_URL = `${API_HOST}${API_ENTRY_POINT}`
 
 export const api = path => {
   return axios(`${BASE_URL}/${path}`)
