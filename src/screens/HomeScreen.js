ab700bd2d35442f6ed092027695f3d4ef5b2bafc
kang yoosam <yooska14@gmail.com>
2019-01-09 18:00:37 +0900

    add global vars to eslintrc

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -15,7 +15,41 @@ import { MonoText } from '../components/StyledText'
 export default class HomeScreen extends React.Component {
   static navigationOptions = {
     header: null,
-  };
+  }
+
+  handleLearnMorePress = () => {
+    WebBrowser.openBrowserAsync('https://docs.expo.io/versions/latest/guides/development-mode')
+  }
+
+  handleHelpPress = () => {
+    WebBrowser.openBrowserAsync(
+      'https://docs.expo.io/versions/latest/guides/up-and-running.html#can-t-see-your-changes',
+    )
+  }
+
+  maybeRenderDevelopmentModeWarning() {
+    if (__DEV__) {
+      const learnMoreButton = (
+        <Text onPress={this.handleLearnMorePress} style={styles.helpLinkText}>
+          Learn more
+        </Text>
+      )
+
+      return (
+        <Text style={styles.developmentModeText}>
+          Development mode is enabled, your app will be slower but you can use useful development
+          tools.
+          {' '}
+          {learnMoreButton}
+        </Text>
+      )
+    }
+    return (
+      <Text style={styles.developmentModeText}>
+        You are not in development mode, your app will run at full speed.
+      </Text>
+    )
+  }
 
   render() {
     return (
@@ -33,7 +67,7 @@ export default class HomeScreen extends React.Component {
           </View>
 
           <View style={styles.getStartedContainer}>
-            {this._maybeRenderDevelopmentModeWarning()}
+            {this.maybeRenderDevelopmentModeWarning()}
 
             <Text style={styles.getStartedText}>Get started by opening</Text>
 
@@ -47,7 +81,7 @@ export default class HomeScreen extends React.Component {
           </View>
 
           <View style={styles.helpContainer}>
-            <TouchableOpacity onPress={this._handleHelpPress} style={styles.helpLink}>
+            <TouchableOpacity onPress={this.handleHelpPress} style={styles.helpLink}>
               <Text style={styles.helpLinkText}>Help, it didn’t automatically reload!</Text>
             </TouchableOpacity>
           </View>
@@ -63,40 +97,6 @@ export default class HomeScreen extends React.Component {
       </View>
     )
   }
-
-  _maybeRenderDevelopmentModeWarning() {
-    if (__DEV__) {
-      const learnMoreButton = (
-        <Text onPress={this._handleLearnMorePress} style={styles.helpLinkText}>
-          Learn more
-        </Text>
-      )
-
-      return (
-        <Text style={styles.developmentModeText}>
-          Development mode is enabled, your app will be slower but you can use useful development
-          tools.
-          {' '}
-          {learnMoreButton}
-        </Text>
-      )
-    }
-    return (
-      <Text style={styles.developmentModeText}>
-          You are not in development mode, your app will run at full speed.
-      </Text>
-    )
-  }
-
-  _handleLearnMorePress = () => {
-    WebBrowser.openBrowserAsync('https://docs.expo.io/versions/latest/guides/development-mode')
-  };
-
-  _handleHelpPress = () => {
-    WebBrowser.openBrowserAsync(
-      'https://docs.expo.io/versions/latest/guides/up-and-running.html#can-t-see-your-changes',
-    )
-  };
 }
 
 const styles = StyleSheet.create({
